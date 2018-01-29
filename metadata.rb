76c50d0e5cfe7c1599b4cdc1f9e2ae9e661e0411
am185238 <alex.munoz@ncr.com>
2018-01-29 00:54:18 -0800

    chef_version change

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Encapsulates common zip functions'
 long_description 'Encapsulates common zip functions'
 source_url 'https://github.com/amunoz951/zipr' if respond_to?(:source_url)
 issues_url 'https://github.com/amunoz951/zipr/issues' if respond_to?(:issues_url)
-chef_version >= 12
+chef_version '>= 12'
 version '1.0.0'
 
 supports 'windows'
