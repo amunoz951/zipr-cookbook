2134810e9d8709bc68228ba07888eda692c65e2f
Munoz, Alex <Alex.Munoz@ncr.com>
2018-08-24 01:08:44 -0700

    Add build-essential dependency for linux

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,12 @@ maintainer_email 'amunoz951@gmail.com'
 license 'Apache-2.0'
 description 'Encapsulates common zip functions'
 long_description 'Encapsulates common zip functions'
-source_url 'https://github.com/amunoz951/zipr' if respond_to?(:source_url)
-issues_url 'https://github.com/amunoz951/zipr/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/amunoz951/zipr'
+issues_url 'https://github.com/amunoz951/zipr/issues'
 chef_version '>= 12'
-version '1.1.1'
+version '1.1.2'
 
 supports 'windows'
 supports 'centos'
+
+depends 'build-essential'
