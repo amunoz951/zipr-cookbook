864771ec74ffaebde6c3129762aab7bc077404e1
Munoz, Alex <amunoz951@gmail.com>
2020-05-27 11:29:44 -0700

    Use specific gem versions since Chef won't support fuzzy versioning until 16.2

--- recipes/gems.rb
+++ recipes/gems.rb
@@ -15,23 +15,20 @@ end
 include_recipe 'zipr::seven_zip_ruby'
 
 required_gems = {
-  'os' => '~> 1.1', # dependency of zipr gem
-  'tzinfo' => '~> 2.0', # dependency of easy_time
-  'tzinfo-data' => '~> 1.2019', # dependency of easy_time
-  'hashly' => '~> 0.1', # dependency of easy_json_config
-  'easy_json_config' => '~> 0.3', # dependency of easy_io
-  'easy_time' => '~> 0.1', # dependency of easy_io
-  'logger' => '~> 1.4', # dependency of easy_io
-  'open3' => '~> 0.1', # dependency of easy_io
-  'easy_io' => '~> 0.4', # dependency of zipr gem
-  'easy_format' => '~> 0.2', # dependency of zipr gem
-  'zipr' => '~> 0.2',
+  'os' => '1.1.0', # dependency of zipr gem
+  'hashly' => '0.1.0', # dependency of easy_json_config
+  'easy_json_config' => '0.3.0', # dependency of easy_io
+  'logger' => '1.4.2', # dependency of easy_io
+  'open3' => '0.1.0', # dependency of easy_io
+  'easy_format' => '0.2.0', # dependency of easy_io gem
+  'easy_io' => '0.4.1', # dependency of zipr gem
+  'zipr' => '0.2.2',
 }
 required_gems.each do |gem_name, gem_version|
   chef_gem gem_name do
     version gem_version
     compile_time true
-    action gem_version.nil? || gem_version.include?('>') ? :upgrade : :install
+    action :install
   end
 end
 
