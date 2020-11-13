cd1e5b6ee1460d796838e3b5152ba8d6bba41503
Munoz, Alex <amunoz951@gmail.com>
2020-11-12 16:46:45 -0800

    bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Provides idempotent compression and extraction resources for zip an
 source_url 'https://github.com/amunoz951/zipr'
 issues_url 'https://github.com/amunoz951/zipr/issues'
 chef_version '>= 12'
-version '3.0.7'
+version '3.0.8'
 
 supports 'windows'
 supports 'centos' # Chef-client version '>= 14.14' appears to break building the native gem extensions for seven_zip_ruby
