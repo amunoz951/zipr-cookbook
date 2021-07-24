77c4ef0e4f2885fccfe713c75ac15547087978d3
Munoz, Alex <amunoz951@gmail.com>
2021-07-24 00:44:23 -0700

    Bump zipr gem for Regexp implementation

--- recipes/dependencies.rb
+++ recipes/dependencies.rb
@@ -21,7 +21,7 @@ required_gems = {
   'open3' => '0.1.0', # dependency of easy_io
   'easy_format' => '0.2.0', # dependency of easy_io gem
   'easy_io' => '0.4.2', # dependency of zipr gem
-  'zipr' => '0.3.1',
+  'zipr' => '0.3.2',
 }
 required_gems.each do |gem_name, gem_version|
   chef_gem gem_name do
