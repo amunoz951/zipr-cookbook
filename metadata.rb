7cd1a4baa30d9232d6fa36be3f2c27bb65bb454b
Munoz, Alex <amunoz951@gmail.com>
2022-07-07 17:49:35 -0700

    Remove .foodcritic file (foodcritic is deprecated)

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Provides idempotent compression and extraction resources for zip an
 source_url 'https://github.com/amunoz951/zipr'
 issues_url 'https://github.com/amunoz951/zipr/issues'
 chef_version '>= 12'
-version '4.1.1'
+version '5.0.0'
 
 supports 'windows'
 supports 'centos' # Chef-client version '>= 14.14' appears to break building the native gem extensions for seven_zip_ruby
